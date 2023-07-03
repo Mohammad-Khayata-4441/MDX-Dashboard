ef5657e150b4dcda3d2c4cd5e9dcd1c788edabb4
mohammad khayata <mohammad_khayata@outlook.com>
2023-07-03 20:10:43 +0300

    fix ts bug

--- src/shared/types/navigation.ts
+++ src/shared/types/navigation.ts
@@ -1,7 +1,8 @@
+import { IconProps } from "@mui/material";
 import { FC } from "react";
-
+import { IconBaseProps, IconType } from 'react-icons'
 export interface NavigationRecord {
-    text:string,
-    path:string,
-    icon:FC,
+    text: string,
+    path: string,
+    icon: FC<IconBaseProps | IconProps>,
 }
